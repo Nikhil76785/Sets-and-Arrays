282e545ce2d2d50f90a494bbf2aab218f7fdd3cd
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-25 19:48:44 +0530

    SET OPERATIONS

---
+++ set1.py
@@ -0,0 +1,17 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.1, "Apple", (1, 2, 3)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+my_set = set([1, 2, 2, 3, 5])
+print(my_set)
+
+num_set = set([2, 3, 5, 4, 6])
+print("The original set: ", num_set)
+
+num_set.pop()
+print("The new set: ", num_set)
