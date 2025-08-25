d49aa6b8da0ccbea78256ba925afec3b7bb792bf
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-25 20:11:56 +0530

    SYMMETRIC DIFFERENCE

---
+++ hw.py
@@ -0,0 +1,9 @@
+set1 = {"Blue", "Green"}
+set2 = {"Blue", "Yellow"}
+set12 = set1.symmetric_difference(set2)
+print(set12)
+
+set3 = {1, 2, 3, 4, 5}
+set4 = {1, 5, 6, 7, 8, 9}
+set34 = set3.symmetric_difference(set4)
+print(set34)
