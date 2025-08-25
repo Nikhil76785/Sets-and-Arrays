3eafb5939580602162171c8394293e943d369d48
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-25 19:54:00 +0530

    INTERSECTION METHOD

---
+++ set2.py
@@ -0,0 +1,7 @@
+set_x = {"Green", "Blue"}
+set_y = {"Yellow", "Blue"}
+print("Original set elements: ", set_x)
+print("Original set elements: ", set_y)
+print("Intersection of 2 said sets: ")
+set_z = set_x.intersection(set_y)
+print(set_z)
